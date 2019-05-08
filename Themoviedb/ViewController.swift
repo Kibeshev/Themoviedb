cddadcae5723166009896c9d9f9bbd77f6cca96e
KBSHV <kibeshev889@gmail.com>
2019-05-08 18:34:10 +0300

    added title

--- Themoviedb/ViewController.swift
+++ Themoviedb/ViewController.swift
@@ -8,21 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+ 
+    @IBOutlet weak var tableView: UITableView!
+    
     let manager = MoviesAPIManager()
     var movies: [Movie] = []
+    
     let urlString = "https://api.themoviedb.org/3/movie/popular?api_key=4cb1eeab94f45affe2536f2c684a5c9e"
-
+// large title нужно сделать
+//    static let largeTitle: UIFont.TextStyle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        manager.getMovie(urlString: urlString) { (getPopularMoviesResponse) in
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        manager.getMovie(urlString: urlString, completion:  { getPopularMoviesResponse in
             DispatchQueue.main.async {
-                print(getPopularMoviesResponse)
+                self.movies = getPopularMoviesResponse?.results ?? []
+                self.tableView.reloadData()
+            
             }
-        }
+        })
+            
+
+        // large title похоже сделал 
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+        UINavigationBar.appearance().prefersLargeTitles = true
+        UINavigationBar.appearance().largeTitleTextAttributes =
+            [NSAttributedString.Key.foregroundColor: UIColor.blue,
+             NSAttributedString.Key.font: UIFont(name: "Papyrus", size: 30) ??
+                UIFont.systemFont(ofSize: 30)]
+        title = "Popular"
 
         // Do any additional setup after loading the view.
+      
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TestCell", for: indexPath) as? CellMovies
+        let movies = self.movies[indexPath.row]
+        cell?.labelTitle.text = movies.original_title
+        return cell ?? UITableViewCell()
     }
 
 
