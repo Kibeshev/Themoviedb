0cbf4663651539659366048d327e6e9c5e7402ef
KBSHV <kibeshev889@gmail.com>
2019-07-16 13:43:45 +0300

    added Search Controller in Search Movies ViewController

--- Themoviedb/SearchMoviesViewController.swift
+++ Themoviedb/SearchMoviesViewController.swift
@@ -10,8 +10,35 @@ import UIKit
 
 class SearchMoviesViewController: UIViewController {
 
+    var moviesViewController: MoviesViewController?
+    @IBOutlet private weak var tableView: UITableView?
+    var searchArray: [Movie] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView?.delegate = self
+        tableView?.dataSource = self
+        tableView?.tableFooterView = UIView()
+        let searchViewController = UISearchController(searchResultsController: moviesViewController)
+        navigationItem.searchController = searchViewController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationItem.searchController?.dimsBackgroundDuringPresentation = false
+    }
+}
+
+extension SearchMoviesViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchArray.count
+    }
+}
+
+extension SearchMoviesViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "TestCellSearch", for: indexPath
+            ) as? SearchMoviesTableViewCell
+        return cell ?? UITableViewCell()
 
     }
 }
